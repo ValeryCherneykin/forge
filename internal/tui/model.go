5d15675e822125a35caea65172b6653d487e288c
Valery Cherneykin <chernecin@icloud.com>
2025-05-16 20:07:23 +0300

    Added func for model Init, Update, View

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,6 +1,9 @@
 package tui
 
-import "github.com/ValeryCherneykin/forge/internal/templates"
+import (
+	"github.com/ValeryCherneykin/forge/internal/templates"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 type Model struct {
 	templates []string
@@ -12,4 +15,46 @@ func NewModel() Model {
 	return Model{
 		templates: templates,
 	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.templates)-1 {
+				m.cursor++
+			}
+		case "enter", "p":
+			// TODO: copy templates
+		}
+	}
+	return m, nil
+}
+
+func (m Model) View() string {
+	s := "Forge \n\n"
+	if len(m.templates) == 0 {
+		s += "None found in ~/.forge/templates/."
+	} else {
+		for i, template := range m.templates {
+			cursor := "  "
+			if m.cursor == i {
+				cursor = "➜ "
+			}
+			s += cursor + template + "\n"
+		}
+	}
+	s += "\nq — exit, j/k — up/down"
+	return s
 }
